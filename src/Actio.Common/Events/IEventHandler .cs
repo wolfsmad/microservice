c2abd22a1fbd8b6228fdff00c18292ab19170cf9
wolf <wolf@DESKTOP-BOQVI47>
2022-12-20 23:08:51 +0330

    12

--- src/Actio.Common/Events/IEventHandler .cs
+++ src/Actio.Common/Events/IEventHandler .cs
@@ -7,6 +7,7 @@ namespace Actio.Common.Events
 {
     public interface IEventHandler<in T> where T:IEvent
     {
+        Task HandleAsync<TEvent>(TEvent msg) where TEvent : IEvent;
         Task HanldeAsync(T @event);
     }
 }
